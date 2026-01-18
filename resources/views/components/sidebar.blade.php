410ef2364700f531b3fc31738085cd46371a4fce
Prakash Pandey <prakashkpandey2059@gmail.com>
2026-01-18 23:02:00 +0545

    make update

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -165,7 +165,7 @@
                 </form>
             </div>
             
-            <!-- Modal Footer -->
+            
             <div class="px-6 py-4 border-t border-gray-200 dark:border-gray-700 flex justify-end space-x-3">
                 <button type="button" onclick="closeModal('create-client-modal')"
                         class="px-4 py-2 border border-gray-300 dark:border-gray-600 text-gray-700 dark:text-gray-300 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700 transition-colors">
